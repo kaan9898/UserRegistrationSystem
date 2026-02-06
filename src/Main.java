7fac7e00e5e63f03c32dab6496c133aa85616ffb
kaan9898 <kaankarahan98@gmail.com>
2026-02-06 09:44:12 +0200

    Main file is commited.

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        RegistrationSystem system = new RegistrationSystem();
+        system.processRegistrationFile("./src/users.txt");
+    }
+}
